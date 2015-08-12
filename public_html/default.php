97ad46e89ea143b62ff51a86fb78e396152102c1
YEZI <kimyj9501@hanmail.net>
2015-08-12 17:47:06 +0900

    server source code

---
+++ public_html/default.php
@@ -0,0 +1,21 @@
+﻿<?
+header('Content-Type: text/html; charset=utf-8');
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+
+    <head>
+
+        <title>호스팅 첫 페이지</title>
+
+    </head>
+
+    <body>
+
+        Hostinger 첫 페이지 !
+
+    </body>
+
+</html>
